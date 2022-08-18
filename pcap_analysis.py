259d29173a3e07e92334b861f7fdd6bee8ad59e3
yogesh <yashasyogesh117@gmail.com>
2022-08-18 20:20:55 +0530

    new

--- pcap_analysis.py
+++ pcap_analysis.py
@@ -2,16 +2,19 @@ from scapy.utils import RawPcapReader
 from scapy.layers.l2 import Ether
 from scapy.all import *
 from scapy.layers.inet import IP, TCP, UDP
+import json 
 
+jmap={"maps":[]}
 
-# IP characteristics
-ip_version = ihl = tos = ilen = id = iflags = frag = ttl = proto = ichksum = src = dst = ioptions = ""
-# TCP characteristics
-tsport = tdport = seq = ack = dataofs = reserved = window = tflags = tchksum = urgptr = toptions = ""
-tflag_fin = tflag_syn = tflag_rst = tflag_psh = tflag_ack = tflag_urg = tflag_ece = tflag_cwr = ""
+def make_json(maps):
+	for i in maps:
+		src,dst=i.split("-")
+		data={"src":src,"dst":dst}
+		jmap["maps"].append(data)
+	host_map=json.dumps(jmap)
+	with open("host_map.json","w") as f:
+		f.write(host_map)
 
-# UDP characteristics
-usport = udport = ulen = uchksum = ""
 
 def analyze_pcap(file):
 	pkts = rdpcap(file)
@@ -23,6 +26,6 @@ def analyze_pcap(file):
 			if(str(str(pkt[IP].src)+"-"+str(pkt[IP].dst)) not in maps and str(str(pkt[IP].dst)+"-"+str(pkt[IP].src)) not in maps):
 				sent=str(pkt[IP].src)+"-"+str(pkt[IP].dst)
 				maps.append(sent)
-	print(maps)
+	make_json(maps)
 
 analyze_pcap("packets.pcap")
--- scan_hosts.py
+++ scan_hosts.py
@@ -1,10 +1,6 @@
 import nmap
-import threading
+import json
 
-# for keys in nm[x]:
-# 				print(keys,nm[x][keys])
-
-all_hosts = ""
 def nmap_scan_classA(sub_host):
 	nm=nmap.PortScanner()
 	print("starting"+str(sub_host))
@@ -16,24 +12,38 @@ def nmap_scan_classA(sub_host):
 	#all_hosts=keyvalues
 	
 
-def scan_host_all():
+def scan_host_all(class_type):
 	classA = '10.0.0.0','10.255.255.255'
 	classB = '172.16.0.0','172.31.255.255'
 	classC = '192.168.0.0','192.168.255.255'
-	for i in range(0,15):
-		ti = threading.Thread(target=nmap_scan_classA, args=(i,))
-		ti.start()
-	print(all_hosts)
+	if(class_type=="A"):
+		res = nm.scan(hosts='10.10-255.10-255.10-255/8', arguments='-O -v')
+	if(class_type=="B"):
+		res = nm.scan(hosts='172.16-31.0-255.0-255/16', arguments='-O -v')
+	if(class_type=="C"):
+		res = nm.scan(hosts='192.168.0-255.0-255', arguments='-O -v')
+	for x in nm.all_hosts():
+		if(nm[x]['status']['state']=="up"):
+			f=open("single_host_"+class_type+"_info.json","w")
+			for x in nm.all_hosts():
+				f.write(nm[x])
+				f.close()
 			
 def scan_host_range(start,end):
 	nm=nmap.PortScanner()
 	res = nm.scan(hosts='192.168.0.0/24', arguments='-O -v')
-	#print(res)
 	for x in nm.all_hosts():
 		if(nm[x]['status']['state']=="up"):
 			print("\n"+x+"\n")
 			for keys in nm[x]:
 				print(keys,nm[x][keys])
 
+def scan_single_host(host):
+	nm=nmap.PortScanner()
+	res=nm.scan(hosts=host, arguments='-O -v')
+	print(nm[host])
+	f=open("single_host_"+host+"_info.json","w")
+	f.write(str(nm[host]).replace("'", "\""))
+	f.close()
 
-scan_host_all()
+scan_single_host("192.168.0.105")
